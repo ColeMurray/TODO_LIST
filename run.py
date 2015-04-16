ee625e6fb9fbad04fa7a716e2d09dd98a43602a0
Cole Murray <cmurr002@ucr.edu>
2015-04-15 20:12:38 -0700

    Fix app run for heroku

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #!venv/bin/python
+import os
 from backend import app
 
 
 if (__name__) == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
+    
